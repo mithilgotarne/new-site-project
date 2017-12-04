79f4aa354f765e540d7fe99fb48ee796778980bf
mithishri <mithilgotarne@gmail.com>
2017-12-05 01:42:17 +0530

    bug fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAUr2dzwNmpT11i8qnRxnroPGN0BfeNSKE",
+    authDomain: "amit-ghoda.firebaseapp.com",
+    databaseURL: "https://amit-ghoda.firebaseio.com",
+    projectId: "amit-ghoda",
+    storageBucket: "amit-ghoda.appspot.com",
+    messagingSenderId: "873816394205"
+  }
 };
