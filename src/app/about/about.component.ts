ab6867c841f48cddfd27d6841d8f49d1c4386a9a
mithil <mithilgotarne@gmail.com>
2018-03-24 10:28:28 +0530

    changes in data

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, OnDestroy,trigger, style, state, transition, animate, keyframes } from '@angular/core';
+import { Component, OnInit, OnDestroy, trigger, style, state, transition, animate, keyframes } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { InfoService } from '../shared/info.service';
 declare var $: any;
 
 @Component({
-    selector: 'mg-about',
-    template: `
+  selector: 'mg-about',
+  template: `
      <div class="container-fluid">
 
         <div class="row body-container" id="about-container">
@@ -34,13 +34,13 @@ declare var $: any;
             <div class="tab-content col-md-10">
 
 
-                <div *ngFor="let a of abouts"
+                <div clas="font-20" *ngFor="let a of abouts"
                         [ngClass]="{active : a.id == activeId, in : a.id == activeId}"
                         role="tabpanel" [@fadeIn]="a.id == activeId ? 'active' : 'inactive'"
                         class="tab-pane"
                         [innerHTML]="a.desc"
                         [attr.id]="a.id">
-                    
+
                 </div>
 
             </div>
@@ -48,58 +48,58 @@ declare var $: any;
 
     </div>
   `,
-    styleUrls: ['./about.component.scss'],
-    animations: [
-        trigger('fadeIn', [
+  styleUrls: ['./about.component.scss'],
+  animations: [
+    trigger('fadeIn', [
 
-            transition('* => active', [
+      transition('* => active', [
 
-                animate(1000, keyframes([
+        animate(1000, keyframes([
 
-                    style({ opacity: 0, offset: 0 }),
-                    style({ opacity: 0.5, offset: .5 }),
-                    style({ opacity: 1, offset: 1 })
+          style({ opacity: 0, offset: 0 }),
+          style({ opacity: 0.5, offset: .5 }),
+          style({ opacity: 1, offset: 1 })
 
-                ]))
-            ])
-        ])
-    ]
+        ]))
+      ])
+    ])
+  ]
 })
 export class AboutComponent implements OnInit, OnDestroy {
-    
 
-    abouts = [];
-    activeId = '';
 
-    constructor(private _router: Router,
-        private _route: ActivatedRoute,
-        private _info: InfoService) { }
+  abouts = [];
+  activeId = '';
 
-    ngOnInit() {
-        window.scrollTo(0, 0);
-        //$('#about-container').css('min-height', ($('.content').height()+20)+'px');
-        $('body').addClass('body-container').removeClass('body-bg');
-        $('#footer-top').addClass('container-fluid').removeClass('container');
-        this._info.getAbouts().subscribe(
-            abouts => this.abouts = abouts,
-            null,
-            () => {
-                this._route.params.subscribe(params => this.activeId = params['id'] || 'know-me');
-                $(window).scroll();
-            }
-        );
-    }
+  constructor(private _router: Router,
+    private _route: ActivatedRoute,
+    private _info: InfoService) { }
 
-    ngOnDestroy(): void {
-        $('#footer-top').addClass('container').removeClass('container-fluid');
-        $('body').removeClass('body-container').addClass('body-bg');
-        //$('body').css('background-color', '#');
-        //$('#about-container').css('min-height', '0px');
-        //console.log("called")
-    }
+  ngOnInit() {
+    window.scrollTo(0, 0);
+    //$('#about-container').css('min-height', ($('.content').height()+20)+'px');
+    $('body').addClass('body-container').removeClass('body-bg');
+    $('#footer-top').addClass('container-fluid').removeClass('container');
+    this._info.getAbouts().subscribe(
+      abouts => this.abouts = abouts,
+      null,
+      () => {
+        this._route.params.subscribe(params => this.activeId = params['id'] || 'know-me');
+        $(window).scroll();
+      }
+    );
+  }
 
-    onClick(id: string) {
-        this._router.navigate(['/about', id]);
-    }
+  ngOnDestroy(): void {
+    $('#footer-top').addClass('container').removeClass('container-fluid');
+    $('body').removeClass('body-container').addClass('body-bg');
+    //$('body').css('background-color', '#');
+    //$('#about-container').css('min-height', '0px');
+    //console.log("called")
+  }
+
+  onClick(id: string) {
+    this._router.navigate(['/about', id]);
+  }
 
 }
